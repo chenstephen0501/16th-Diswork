929a3f83d5ffef9bce02716c1c23a9eda878fae2
stephen0501 <tom9876555@gmail.com>
2024-07-21 01:22:47 +0800

    modify Alpine mounts version 3.x.x. After new comments are added to the article, the number of comments in the article will be updated simultaneously.

--- static/scripts/comment.js
+++ static/scripts/comment.js
@@ -1,24 +1,21 @@
-document.getElementById('comment-form').addEventListener('submit', function (event) {
-    event.preventDefault();
+function disableButton(event) {
+  event.target.querySelector("button").disabled = true;
+}
 
-    let formData = new FormData(this);
-    let commentUrl = document.getElementById('comment-url').value;
-
-    fetch(commentUrl, {
-        method: 'POST',
-        body: formData,
-        headers: {
-            'Accept': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-CSRFToken': '{{ csrf_token }}'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('comments-container').innerHTML = data.comment_html;
-            document.getElementById('comment-form').reset();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+document.addEventListener("alpine:init", () => {
+    Alpine.store("commentCount", {
+      count: 1,
+      increment() {
+        this.count++;
+      },
+    });
 });
+
+function commentCountComponent(initialCount) {
+  return {
+    init() {
+      this.$store.commentCount.count = initialCount
+    }
+  }
+
+}
