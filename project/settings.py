953ee2c435af2f296a7508323b29649489805764
stephen0501 <tom9876555@gmail.com>
2024-09-04 17:27:37 +0800

    fix add cors

--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,7 @@ HOST_NAME = os.getenv("HOST_NAME")
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [f"https://{HOST_NAME}", f"http://{HOST_NAME}"]
+CSRF_TRUSTED_ORIGINS = [f"https://{HOST_NAME}", f"http://{HOST_NAME}", "https://diswork-f0115f5426e3.herokuapp.com"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     "channels",
     "ads",
     "notifications",
+    'corsheaders',
 ]
 
 SOCIALACCOUNT_LOGIN_ON_GET = True
@@ -91,6 +92,7 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -223,4 +225,7 @@ AUTHENTICATION_BACKENDS = {
 }
 
 SITE_ID = int(os.getenv("SITE_ID", 1))
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 LOGIN_REDIRECT_URL = "/"
