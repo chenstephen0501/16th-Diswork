ac02fc9504472f029e5a8d3a8179f7bf9aa47d9b
stephen0501 <tom9876555@gmail.com>
2024-09-04 09:13:58 +0800

    fix: settings.py

--- staticfiles/scripts/add_private_room.js
+++
@@ -1,9 +0,0 @@
-const roomNameInputs = document.querySelectorAll(".room-name-input")
-const roomNameSubmits = document.querySelectorAll(".room-name-submit")
-    roomNameSubmits.forEach((submitButton, index) => {
-        submitButton.addEventListener("click", function() {
-        const roomName = roomNameInputs[index].value.split(',').sort((a, b) => (a - b))
-        const newRoomName = roomName[0] + "_" + roomName[1]
-        window.location.pathname = '/chat/' + newRoomName + '/';
-        })
-})
--- staticfiles/scripts/csrf.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("htmx:configRequest", function (event) {
-  const csrfToken = document
-    .querySelector('meta[name="csrf-token"]')
-    .getAttribute("content");
-  if (csrfToken) {
-    event.detail.headers["X-CSRFToken"] = csrfToken;
-  }
-});
