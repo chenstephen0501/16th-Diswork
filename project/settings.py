f0705e6e1b489930dc234de58f28c0069239f1a0
stephen0501 <tom9876555@gmail.com>
2024-09-04 10:44:06 +0800

    fix: settings.py

--- project/settings.py
+++ project/settings.py
@@ -195,11 +195,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # S3
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
